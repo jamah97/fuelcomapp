4bcf7c68bcacfc802dd886c09780f3cbfa8fbd0c
jamah97 <88891435+jamah97@users.noreply.github.com>
2022-03-25 13:00:53 -0500

    Update fuelcomapp.py

--- fuelcomapp.py
+++ fuelcomapp.py
@@ -98,16 +98,10 @@ def main():
 	st.write(df.head())
 
 	if st.button("Numerical anaysis"):
-	    c1, c2 = st.columns(2)
-	    with cl:
-                with st.expander('Statistical Summary'):
-			st.write(df.describe())
-		    # st.write('Here is a statistical summary of the numerical values in the dataset')
-		      #st.write(df.describe())
-	    with c2:
-	        with st.expander('Correlation Summary'):
-		    # st.write('Here is a correlation summary of the numerical values in the dataset')
-		   st.write(df.corr())
+	    st.write("Numerical summary")
+	    st.write(df.describe())
+	    st.write("Numerical corrlation")
+	    st.write(df.corr())
 
 	cv= df2.columns.tolist()
 	#cv1= df1.columns.tolist()
